37e9a955db8040085a217c20cc4196cb7fba479e
ftwpokedad <146678808+ftwpokedad@users.noreply.github.com>
2025-04-24 21:12:28 -0400

    Initial Commit

---
+++ VGCTournamentCalc/VGCTournamentCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  VGCTournamentCalcApp.swift
+//  VGCTournamentCalc
+//
+//  Created by Michael Woitalla on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct VGCTournamentCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
